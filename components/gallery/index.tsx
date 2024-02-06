6adfe6ede1d75df1b456b8c667e16df657eb9068
Kevin Muñoz <harquin09@gmail.com>
2024-02-06 12:43:03 -0500

    fist

--- components/gallery/index.tsx
+++ components/gallery/index.tsx
@@ -11,10 +11,8 @@ interface GalleryProps {
   images: Image[];
 }
 
-const Gallery: React.FC<GalleryProps> = ({
-  images = []
-}) => {
-  return ( 
+const Gallery: React.FC<GalleryProps> = ({ images = [] }) => {
+  return (
     <Tab.Group as="div" className="flex flex-col-reverse">
       <div className="mx-auto mt-6 hidden w-full max-w-2xl sm:block lg:max-w-none">
         <Tab.List className="grid grid-cols-4 gap-6">
@@ -27,18 +25,13 @@ const Gallery: React.FC<GalleryProps> = ({
         {images.map((image) => (
           <Tab.Panel key={image.id}>
             <div className="aspect-square relative h-full w-full sm:rounded-lg overflow-hidden">
-              <NextImage
-                fill
-                src={image.url}
-                alt="Image"
-                className="object-cover object-center"
-              />
+              <NextImage fill src={image.url} alt="Image" className="" />
             </div>
           </Tab.Panel>
         ))}
       </Tab.Panels>
     </Tab.Group>
   );
-}
- 
+};
+
 export default Gallery;
